755dc896eacb2912e319999fa64344682e4e9b96
aleksvin8888 <aleksvin8888@gmail.com>
2021-01-07 22:34:51 +0200

    commit push to gitHab

--- testGit.php
+++ testGit.php
@@ -1,18 +1,5 @@
 <?php
 	echo "string tect ";
 	echo "string test ";
-?>
-<?php
-	echo "string tect ";
-	echo "альтернатива 2";
-	echo " test ";
-?>
-<?php
-	echo "string tect ";
-	echo "altern 2 ";
-	echo "string test ";
-?>
-<?php
-	echo "string tect ";
-	echo "string test ";
+	echo "push to GitHub ";
 ?>
