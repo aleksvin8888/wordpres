b3b96d6b43714ab803dcaf64f765476ad21c82c3
aleksvin8888 <aleksvin8888@gmail.com>
2021-01-07 18:13:28 +0200

    commit test  redacktor

--- testGit.php
+++ testGit.php
@@ -2,3 +2,15 @@
 	echo "string tect ";
 	echo "string test ";
 ?>
+<?php
+	echo "string tect ";
+	echo "string test ";
+?>
+<?php
+	echo "string tect ";
+	echo "string test ";
+?>
+<?php
+	echo "string tect ";
+	echo "string test ";
+?>
